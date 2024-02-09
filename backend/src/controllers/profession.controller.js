7c2b65e2d5e18ab1d608a6b468f99e9b3efc5a9d
Facundo Romero <59918131+BarrierHT@users.noreply.github.com>
2024-02-09 07:41:42 -0300

    Applicants endpoint finished
    Every applicant was populated with its professions

--- backend/src/controllers/profession.controller.js
+++ backend/src/controllers/profession.controller.js
@@ -4,13 +4,13 @@ const Profession = require('../app').models.professions;
 exports.getProfessions = async (req, res, next) => {
 	try {
 		// Consulta todas las profesiones
-		const profesiones = await Profession.findAll();
+		const professions = await Profession.findAll();
 
-		if (!profesiones)
+		if (!professions)
 			throw errorHandler('An error with professions ocurred', 500, {});
 		// Retorna las profesiones en formato JSON
-		res.json(profesiones);
-	} catch (error) {
+		res.json(professions);
+	} catch (err) {
 		next(err);
 	}
 };
